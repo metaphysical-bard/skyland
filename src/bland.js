a12978ba2282be258b367f4fb02ba2c3f2da9c0e
metaphysical-bard <101070rumecome@gmail.com>
2025-02-27 09:06:51 +0900

    bland.js / remake and add test.html

--- src/bland.js
+++ src/bland.js
@@ -5,124 +5,220 @@ bland.js
 */
 
 class bland {
-  space() {
-    switch (this.state) {
-      case 0 :
-        this.log = "error"
-        break
-      case 6 :
-        this.word += " "
-        break
+
+  count(s) {
+    this.pos.index += 1
+    if (s === "\n") {
+      this.pos.row += 1
+      this.pos.col = 0
+    }
+    else { this.pos.col += 1 }
+  }
+
+  error(s = "") {
+    this.log += "error : row " + (this.pos.row + 1) + ", col " + (this.pos.col + 1)
+    this.log += ", symbol : " + this.s[this.pos.index] + "\n"
+    this.log += s
+  }
+
+  symbolCh(s){
+    switch (s) {
+      case "!" : case "\"" : case "#" : case "$" : case "%" : case "&" : case "'" :
+      case "(" : case ")" : case "-" : case "=" : case "~" : case "^" : case "|" :
+      case "@" : case "`" : case "+" : case ";" : case "*" : case ":" : case "[" :
+      case "]" : case "{" : case "}" : case "," : case "." : case "<" : case ">" :
+      case "/" : case "?" : case "\\" : case " " : case "\n" :
+        return true
     }
   }
 
-  leftParenthesis() {
-    switch (this.state) {
-      case 0 :
-        this.obj.type = "function"
-        this.obj.name = this.word
-        this.word = ""
-        this.obj.orders = []
-        this.state = 1
-        break
-      case 3 :
-        this.order[0] = "call"
-        this.order[1] = this.word
-        this.word = ""
-        this.state = 5
-        break
-      default :
-        this.log = "error"
+  comment() {
+    let state = 0
+    while (this.pos.index < this.s.length && this.log === "") {
+      let s = this.s[this.pos.index]
+      switch (state) {
+        case 0 :
+          switch (s) {
+            case "/" :
+              state = 1
+              break
+            case "*" :
+              state = 2
+              break
+            default :
+              this.error("comment")
+              return
+          }
+          break
+        case 1 :
+          if (s === "\n") { return }
+          break
+        case 2 :
+          if (s === "*") { state = 3 }
+          break
+        case 3 :
+          if (s === "/") { return }
+          else { state = 2 }
+      }
+      this.count(s)
     }
+    this.error("comment")
   }
 
-  rightParenthesis() {
-    switch (this.state) {
-      case 1 :
-        this.state = 2
-        break
-      case 8 :
-        this.state = 9
-        this.obj.orders.push(this.order)
-        this.order = []
-        break
-      default :
-        this.log = "error"
+  arguments() {
+    let ary = ["arguments"]
+    let state = 0
+    let word = ""
+    while (this.pos.index < this.s.length && this.log === "") {
+      let s = this.s[this.pos.index]
+      if (this.symbolCh(s) === true) {
+        if (word === "") {
+          if (s === ")") {
+            this.count(s)
+            return ary
+          }
+          this.error("arguments")
+        }
+        else {
+          if (state === 0) {
+            if (s === ")") { this.error("arguments") }
+            else {
+              switch (word) {
+                case "i32" :
+                  ary.push("i32")
+                  break
+                case "f32" :
+                  ary.push("f32")
+                  break
+                case "i64" :
+                  ary.push("i64")
+                  break
+                case "f64" :
+                  ary.push("f64")
+                  break
+                default : this.error("arguments")
+              }
+              state = 1
+              word = ""
+            }
+          }
+          else {
+            ary.push(word)
+            if (s === ")") {
+              this.count(s)
+              return ary
+            }
+            state = 0
+            word = ""
+          }
+        }
+      }
+      else { word += s }
+      this.count(s)
     }
+    this.error("arguments")
   }
 
-  leftCurlyBrace() {
-    switch (this.state) {
-      case 2 :
-        this.state = 3
-        break
-      default :
-        this.log = "error"
-    }
+  set() {
+
   }
 
-  rightCurlyBrace() {
-    switch (this.state) {
-      case 3, 9 :
-        this.list.push(this.obj)
-        this.obj = {}
-        this.state = 0
-        break
-      default :
-        this.log = "error"
+  logString() {
+    let ary = ["call", "log"]
+    let state = 0
+    let word = ""
+    while (this.pos.index < this.s.length && this.log === "") {
+      let s = this.s[this.pos.index]
+      switch (state) {
+        case 0 :
+          if (s === "(") { state = 1 }
+          else { this.error("log") }
+          break
+        case 1 :
+          if (s === "\"") { state = 2 }
+          else { this.error("log") }
+          break
+        case 2 :
+          if (s === "\"") {
+            ary.push(word)
+            return ary
+          }
+          else { word += s }
+      }
+      this.count(s)
     }
+    this.error("log")
+    return false
   }
 
-  doubleQuotation() {
-    switch (this.state) {
-      case 5 :
-        this.state = 6
-        break
-      case 6 :
-        this.order.push(this.word)
-        this.word = ""
-        this.state = 8
-        break
-      default :
-        this.log = "error" + this.state
+  orders() {
+    let ary = []
+    ary.push(this.arguments())
+    let state = 0
+    let word = ""
+    while (this.pos.index < this.s.length && this.log === "") {
+      let s = this.s[this.pos.index]
+      if (this.symbolCh(s) === true) {
+        switch (state) {
+          case 0 :
+            if (s === " " || s === "\n") {
+              if (word !== "") { this.error("orders") }
+            }
+            else if (s === "{") { state = 1 }
+            else { this.error("orders") }
+            break
+          case 1 :
+            if (s === "}") {
+              this.count(s)
+              return ary
+            }
+            else {
+              if (word !== "") {
+                switch (word) {
+                  case "i32" : case "f32" : case "i64" : case "f64" :
+                    ary.push(this.set(word))
+                    word = ""
+                    break
+                  case "log" :
+                    ary.push(this.logString())
+                    word = ""
+                    break
+                  default : { this.error("orders") }
+                }
+              }
+            }
+        }
+      }
+      else { word += s }
+      this.count(s)
     }
+    this.error("orders")
   }
 
   convert(code) {
-    const s = Array.from(code.replace(/\/\/[^\n]*\n|\/\*.*\*\//gu, ""))
+    this.s = Array.from(code)
     this.list = []
-    this.state = 0
-    this.obj = {}
-    this.order = []
-    this.word = ""
+    this.pos = { row : 0, col : 0, index : 0 }
     this.log = ""
-    for (let i = 0; i < s.length; i++) {
-      if (this.log !== "")
-        return { list : this.list, log : this.log }
-      switch (s[i]) {
-        case " " :
-          this.space()
+    let word = ""
+    while (this.pos.index < this.s.length && this.log === "") {
+      let s = this.s[this.pos.index]
+      switch (s) {
+        case "/" :
+          this.count(s)
+          this.comment()
+          word = "", s = "\n"
           break
         case "(" :
-          this.leftParenthesis()
-          break
-        case ")" :
-          this.rightParenthesis()
-          break
-        case "{" :
-          this.leftCurlyBrace()
-          break
-        case "}" :
-          this.rightCurlyBrace()
-          break
-        case "\"" :
-          this.doubleQuotation()
-          break
-        case "\n" :
+          this.count(s)
+          this.list.push({type : "function", name : word, orders : this.orders() })
+          word = "", s = "}"
           break
         default :
-          this.word += s[i]
+          if (this.symbolCh(s) === true) { this.error() }
+          else { word += s }
       }
+      this.count(s)
     }
     return { list : this.list, log : this.log }
   }
